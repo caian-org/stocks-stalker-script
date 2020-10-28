d65742199b33e623def8be33205496c6ccbe40dc
Caian R. Ertl <hi@caian.org>
2020-10-28 06:14:08 -0300

    style: run beautify

--- src/main.ts
+++ src/main.ts
@@ -13,27 +13,27 @@ enum HttpStatus {
   OK = 200,
   BAD_REQUEST = 400,
   UNAUTHORIZED = 401,
-  INTERNAL_ERROR = 500,
+  INTERNAL_ERROR = 500
 }
 
 enum Sheet {
   TICKERS = 'tickers',
   DEBUG = 'debug',
-  TEST = 'test',
+  TEST = 'test'
 }
 
 interface IHash {
-  [key: string]: any;
+  [key: string]: any
 }
 
 interface ITicker {
-  row: number;
-  code: string;
-  isBought: boolean;
-  expBuy?: number;
-  expSell?: number;
-  value?: number;
-  update?: string;
+  row: number
+  code: string
+  isBought: boolean
+  expBuy?: number
+  expSell?: number
+  value?: number
+  update?: string
 }
 
 /* Globals */
@@ -48,12 +48,11 @@ const sheetIsEmpty = () => getActiveSheet().getLastRow() < headerRowsOffset
 
 const errorEvent = (error: Error, eventData: HttpEvent) => ({ error, got: eventData })
 
-const isDebugRequest = (e: HttpEvent) => Object.prototype.hasOwnProperty.call(e.parameter, 'isDebug')
+const isDebugRequest = (e: HttpEvent) =>
+  Object.prototype.hasOwnProperty.call(e.parameter, 'isDebug')
 
 function isAuthorized (e: HttpEvent) {
-  const accessToken = PropertiesService
-    .getScriptProperties()
-    .getProperty('ACCESS_TOKEN')
+  const accessToken = PropertiesService.getScriptProperties().getProperty('ACCESS_TOKEN')
 
   const p = e.parameter as IHash
   return p.accessToken === accessToken
@@ -61,23 +60,21 @@ function isAuthorized (e: HttpEvent) {
 
 function response (status: HttpStatus, data?: any): TextOutput {
   const res: IHash = { status }
-  if (typeof (data) !== 'undefined') {
+  if (typeof data !== 'undefined') {
     res.data = data
   }
 
-  return ContentService
-    .createTextOutput(JSON.stringify(res))
-    .setMimeType(ContentService.MimeType.JSON)
+  return ContentService.createTextOutput(JSON.stringify(res)).setMimeType(
+    ContentService.MimeType.JSON
+  )
 }
 
 /* Sheet manipulation */
 
 const setSheet = (name: string) => SpreadsheetApp.setActiveSheet(document.getSheetByName(name))
 
-const loadSheetCond = (e: HttpEvent) => (
-  isDebugRequest(e)
-    ? setSheet(Sheet.DEBUG)
-    : setSheet(Sheet.TICKERS))
+const loadSheetCond = (e: HttpEvent) =>
+  isDebugRequest(e) ? setSheet(Sheet.DEBUG) : setSheet(Sheet.TICKERS)
 
 function cleanAll (ss: Spreadsheet): void {
   if (sheetIsEmpty()) return
@@ -85,7 +82,7 @@ function cleanAll (ss: Spreadsheet): void {
   const last = ss.getLastRow()
 
   const values: string[][] = []
-  for (let i = 0, l = (last - headerRowsOffset + 1); i < l; i++) {
+  for (let i = 0, l = last - headerRowsOffset + 1; i < l; i++) {
     values.push(['', '', '', '', '', ''])
   }
 
@@ -95,14 +92,7 @@ function cleanAll (ss: Spreadsheet): void {
 function updateSheetContent (ss: Spreadsheet, tickers: ITicker[]): void {
   tickers.forEach((t: ITicker): void => {
     const row = t.row + headerRowsOffset
-    const values = [
-      t.code,
-      (t.isBought ? 'Y' : 'N'),
-      t.expBuy,
-      t.expSell,
-      t.value,
-      t.update
-    ]
+    const values = [t.code, t.isBought ? 'Y' : 'N', t.expBuy, t.expSell, t.value, t.update]
 
     ss.getRange(`C${row}:H${row}`).setValues([values])
   })
@@ -113,21 +103,25 @@ function getSheetContent (ss: Spreadsheet): ITicker[] {
 
   const rows = ss.getRange(`C${headerRowsOffset}:F${ss.getLastRow()}`).getValues()
 
-  return rows.map((row: string[], i: number): ITicker => {
-    const code = row[0]
-    const isBought = row[1] === 'Y'
-    const expBuy = parseFloat(row[2]) || undefined
-    const expSell = parseFloat(row[3]) || undefined
+  return rows.map(
+    (row: string[], i: number): ITicker => {
+      const code = row[0]
+      const isBought = row[1] === 'Y'
+      const expBuy = parseFloat(row[2]) || undefined
+      const expSell = parseFloat(row[3]) || undefined
 
-    return { row: i, code, isBought, expBuy, expSell }
-  })
+      return { row: i, code, isBought, expBuy, expSell }
+    }
+  )
 }
 
 /* HTTP events */
 
 /* eslint-disable-next-line */
 function doGet(e: GetEvent) {
-  if (!isAuthorized(e)) { return response(HttpStatus.UNAUTHORIZED) }
+  if (!isAuthorized(e)) {
+    return response(HttpStatus.UNAUTHORIZED)
+  }
 
   loadSheetCond(e)
   const ss = getActiveSheet()
@@ -147,8 +141,13 @@ function doGet(e: GetEvent) {
 
 /* eslint-disable-next-line */
 function doPost(e: PostEvent) {
-  if (!isAuthorized(e)) { return response(HttpStatus.UNAUTHORIZED) }
-  if (typeof (e.postData) === 'undefined') { return response(HttpStatus.BAD_REQUEST) }
+  if (!isAuthorized(e)) {
+    return response(HttpStatus.UNAUTHORIZED)
+  }
+
+  if (typeof e.postData === 'undefined') {
+    return response(HttpStatus.BAD_REQUEST)
+  }
 
   loadSheetCond(e)
   const ss = getActiveSheet()
